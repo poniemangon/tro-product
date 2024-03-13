d715d1d2593a1bf5a98ee4204da6e8af45b2480f
gon_14 <poniemangon@gmail.com>
2024-03-13 16:19:09 -0300

    frist

---
+++ scripts/slider.js
@@ -0,0 +1,54 @@
+$('.carousel').slick({
+  arrows: true,
+  infinite: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [{
+      breakpoint: 1250,
+      settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+      }
+  },
+  {
+    breakpoint: 980,
+    settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+
+    }
+},
+  {
+      breakpoint: 700,
+      settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+
+      }
+  }]
+});
+
+$('.carousel2').slick({
+    arrows: true,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [{
+        breakpoint: 1250,
+        settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+        }
+    },]
+  });
+
+
